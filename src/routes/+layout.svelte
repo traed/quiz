8bc2562ad3d8d1310dcda906b11fc6db461726f7
Mattias Forsman <forsman1991@gmail.com>
2024-08-24 20:38:48 +0200

    Setup service worker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,24 @@
 <script>
-	import './app.css';
+	import { onMount } from 'svelte';
+	import '../app.css';
+
+	async function detectSWUpdate() {
+		const registration = await navigator.serviceWorker.ready;
+
+		registration.addEventListener('updatefound', () => {
+			const newSW = registration.installing;
+			newSW?.addEventListener('statechange', () => {
+				if (newSW.state === 'installed' && confirm('New update available. Reload to update?')) {
+					newSW.postMessage({ type: 'SKIP_WAITING' });
+					window.location.reload();
+				}
+			});
+		});
+	}
+
+	onMount(() => {
+		detectSWUpdate();
+	});
 </script>
 
 <slot />
